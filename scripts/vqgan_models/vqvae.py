0d25cb1903d24c51959a700cb8aca3e5af2b58bc
keyu-tian <k_code@163.com>
2023-04-10 01:40:11 +0800

    [add] rm the dependency of torch_lightening

--- scripts/vqgan_models/vqvae.py
+++ scripts/vqgan_models/vqvae.py
@@ -1,10 +1,8 @@
 import importlib
 
 import numpy as np
-import pytorch_lightning as pl
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 from .ddpm import Decoder, Encoder
 
@@ -153,7 +151,7 @@ class VectorQuantizer2(nn.Module):
         return z_q
 
 
-class VQModel(pl.LightningModule):
+class VQModel(nn.Module):
     def __init__(self,
                  ddconfig,
                  lossconfig,
@@ -172,7 +170,7 @@ class VQModel(pl.LightningModule):
         self.encoder = Encoder(**ddconfig)
         self.decoder = Decoder(**ddconfig)
         self.loss = instantiate_from_config(lossconfig)
-        self.quantize = VectorQuantizer2(n_embed, embed_dim, beta=0.25,
+        self.quantize: VectorQuantizer2 = VectorQuantizer2(n_embed, embed_dim, beta=0.25,
                                          remap=remap, sane_index_shape=sane_index_shape)
         self.quant_conv = torch.nn.Conv2d(ddconfig["z_channels"], embed_dim, 1)
         self.post_quant_conv = torch.nn.Conv2d(embed_dim, ddconfig["z_channels"], 1)
@@ -208,7 +206,7 @@ class VQModel(pl.LightningModule):
         return dec
     
     def decode_code(self, code_b):
-        quant_b = self.quantize.embed_code(code_b)
+        quant_b = self.post_quant_conv(self.quantize.embedding(code_b)) # todo: self.quantize.embedding(code_b)'s shape could be wrong
         dec = self.decode(quant_b)
         return dec
     
@@ -217,84 +215,14 @@ class VQModel(pl.LightningModule):
         dec = self.decode(quant)
         return dec, diff
     
-    def get_input(self, batch, k):
-        x = batch[k]
-        if len(x.shape) == 3:
-            x = x[..., None]
-        x = x.permute(0, 3, 1, 2).to(memory_format=torch.contiguous_format)
-        return x.float()
-    
-    def training_step(self, batch, batch_idx, optimizer_idx):
-        x = self.get_input(batch, self.image_key)
-        xrec, qloss = self(x)
-        
-        if optimizer_idx == 0:
-            # autoencode
-            aeloss, log_dict_ae = self.loss(qloss, x, xrec, optimizer_idx, self.global_step,
-                                            last_layer=self.get_last_layer(), split="train")
-            
-            self.log("train/aeloss", aeloss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
-            self.log_dict(log_dict_ae, prog_bar=False, logger=True, on_step=True, on_epoch=True)
-            return aeloss
-        
-        if optimizer_idx == 1:
-            # discriminator
-            discloss, log_dict_disc = self.loss(qloss, x, xrec, optimizer_idx, self.global_step,
-                                                last_layer=self.get_last_layer(), split="train")
-            self.log("train/discloss", discloss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
-            self.log_dict(log_dict_disc, prog_bar=False, logger=True, on_step=True, on_epoch=True)
-            return discloss
-    
-    def validation_step(self, batch, batch_idx):
-        x = self.get_input(batch, self.image_key)
-        xrec, qloss = self(x)
-        aeloss, log_dict_ae = self.loss(qloss, x, xrec, 0, self.global_step,
-                                        last_layer=self.get_last_layer(), split="val")
-        
-        discloss, log_dict_disc = self.loss(qloss, x, xrec, 1, self.global_step,
-                                            last_layer=self.get_last_layer(), split="val")
-        rec_loss = log_dict_ae["val/rec_loss"]
-        self.log("val/rec_loss", rec_loss,
-                 prog_bar=True, logger=True, on_step=True, on_epoch=True, sync_dist=True)
-        self.log("val/aeloss", aeloss,
-                 prog_bar=True, logger=True, on_step=True, on_epoch=True, sync_dist=True)
-        self.log_dict(log_dict_ae)
-        self.log_dict(log_dict_disc)
-        return self.log_dict
-    
-    def configure_optimizers(self):
-        lr = self.learning_rate
-        opt_ae = torch.optim.Adam(list(self.encoder.parameters()) +
-                                  list(self.decoder.parameters()) +
-                                  list(self.quantize.parameters()) +
-                                  list(self.quant_conv.parameters()) +
-                                  list(self.post_quant_conv.parameters()),
-                                  lr=lr, betas=(0.5, 0.9))
-        opt_disc = torch.optim.Adam(self.loss.discriminator.parameters(),
-                                    lr=lr, betas=(0.5, 0.9))
-        return [opt_ae, opt_disc], []
-    
-    def get_last_layer(self):
-        return self.decoder.conv_out.weight
-    
-    def log_images(self, batch, **kwargs):
-        log = dict()
-        x = self.get_input(batch, self.image_key)
-        x = x.to(self.device)
-        xrec, _ = self(x)
-        if x.shape[1] > 3:
-            # colorize with random projection
-            assert xrec.shape[1] > 3
-            x = self.to_rgb(x)
-            xrec = self.to_rgb(xrec)
-        log["inputs"] = x
-        log["reconstructions"] = xrec
-        return log
-    
-    def to_rgb(self, x):
-        assert self.image_key == "segmentation"
-        if not hasattr(self, "colorize"):
-            self.register_buffer("colorize", torch.randn(3, x.shape[1], 1, 1).to(x))
-        x = F.conv2d(x, weight=self.colorize)
-        x = 2. * (x - x.min()) / (x.max() - x.min()) - 1.
-        return x
+    # def configure_optimizers(self):
+    #     lr = self.learning_rate
+    #     opt_ae = torch.optim.Adam(list(self.encoder.parameters()) +
+    #                               list(self.decoder.parameters()) +
+    #                               list(self.quantize.parameters()) +
+    #                               list(self.quant_conv.parameters()) +
+    #                               list(self.post_quant_conv.parameters()),
+    #                               lr=lr, betas=(0.5, 0.9))
+    #     opt_disc = torch.optim.Adam(self.loss.discriminator.parameters(),
+    #                                 lr=lr, betas=(0.5, 0.9))
+    #     return [opt_ae, opt_disc], []
