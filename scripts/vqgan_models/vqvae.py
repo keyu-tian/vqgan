6fffa595305055474d96399f07f5da8ef0a806f6
keyu-tian <k_code@163.com>
2023-04-10 01:42:30 +0800

    [upd] rm lossconfig

--- scripts/vqgan_models/vqvae.py
+++ scripts/vqgan_models/vqvae.py
@@ -7,18 +7,18 @@ import torch.nn as nn
 from .ddpm import Decoder, Encoder
 
 
-def get_obj_from_str(string, reload=False):
-    module, cls = string.rsplit(".", 1)
-    if reload:
-        module_imp = importlib.import_module(module)
-        importlib.reload(module_imp)
-    return getattr(importlib.import_module(module, package=None), cls)
-
-
-def instantiate_from_config(config):
-    if not "target" in config:
-        raise KeyError("Expected key `target` to instantiate.")
-    return get_obj_from_str(config["target"])(**config.get("params", dict()))
+# def get_obj_from_str(string, reload=False):
+#     module, cls = string.rsplit(".", 1)
+#     if reload:
+#         module_imp = importlib.import_module(module)
+#         importlib.reload(module_imp)
+#     return getattr(importlib.import_module(module, package=None), cls)
+#
+#
+# def instantiate_from_config(config):
+#     if not "target" in config:
+#         raise KeyError("Expected key `target` to instantiate.")
+#     return get_obj_from_str(config["target"])(**config.get("params", dict()))
 
 
 class VectorQuantizer2(nn.Module):
@@ -154,7 +154,7 @@ class VectorQuantizer2(nn.Module):
 class VQModel(nn.Module):
     def __init__(self,
                  ddconfig,
-                 lossconfig,
+                 # lossconfig,
                  n_embed,
                  embed_dim,
                  ckpt_path=None,
@@ -169,7 +169,7 @@ class VQModel(nn.Module):
         self.image_key = image_key
         self.encoder = Encoder(**ddconfig)
         self.decoder = Decoder(**ddconfig)
-        self.loss = instantiate_from_config(lossconfig)
+        # self.loss = instantiate_from_config(lossconfig)
         self.quantize: VectorQuantizer2 = VectorQuantizer2(n_embed, embed_dim, beta=0.25,
                                          remap=remap, sane_index_shape=sane_index_shape)
         self.quant_conv = torch.nn.Conv2d(ddconfig["z_channels"], embed_dim, 1)
